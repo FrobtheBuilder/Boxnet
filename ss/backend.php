685f55528f3dfb491eb3b0f0eb9c3b16c56d5352
Evan Banyash <evan@banyash.com>
2013-08-22 17:04:58 +0400

    changes

--- ss/backend.php
+++ ss/backend.php
@@ -12,7 +12,7 @@
         public $db;
 
 		function __construct($pname = "default", $pimage = "default.jpg", 
-								$pdesc = "this is a default object", $pvalue = 1, $pnsfw = false) {
+			$pdesc = "this is a default object", $pvalue = 1, $pnsfw = false) {
 			$this->name = $pname;
 			$this->image = $pimage;
 			$this->desc = $pdesc;
@@ -22,14 +22,17 @@
         
         function connect() {
             $this->db = new mysqli("localhost", $user["name"], $user["password"], "boxnet" );
-            $this->db = new mysqli("reda.genderal.org", "boxnet", "whatsinside", "boxnet" );
-            return var_dump($this->db);
         }
         
         function populate($pmethod = "random") {
             switch ($pmethod) {
                 case "random":
+                    $result = $this->db->query("select count(*) from items;");
+                    var_dump($result);
+                    //$count = $result->fetch_object()->
+                    //$result->close();
                     
+                    //$this->db->query("select * from items where id=");
                     break;
             }
         }
@@ -39,7 +42,8 @@
 
 
 	$defaultitem = new Item();
-    echo $defaultitem->connect();
+    $defaultitem->connect();
+    $defaultitem->populate("random");
 	echo json_encode($defaultitem, JSON_NUMERIC_CHECK);
 ?>
 
