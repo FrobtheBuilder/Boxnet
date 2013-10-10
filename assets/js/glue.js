1e862529ecc563f708fb0fb28c73e77bb904dfb9
Evan Banyash <evan@banyash.com>
2013-10-10 21:19:35 +0400

    fixed layout

--- assets/js/glue.js
+++ assets/js/glue.js
@@ -12,14 +12,18 @@ $(document).ready(function() {
 function display (pitem, pelement) {
 	pelement.html("");
 	pelement.append(
-		$("<div></div>").attr("class", "panel").append(
-			$("<img>").attr("class", "image img-rounded").attr("src", imagepath + pitem.image),
-			$("<br>"),
-			$("<h1></h1>").html(pitem.name),
-			$("<br>"),
-			$("<p></p>").html(pitem.desc),
-			$("<br>"),
-			$("<a></a>").attr("class", "btn btn-mini return").attr("href", "index.html").html("return")
+		$("<div></div>").attr("class", "panel panel-default item").append(
+            $("<div></div>").attr("class", "panel-heading").append(
+                $("<img>").attr("class", "image img-rounded").attr("src", imagepath + pitem.image)
+			),
+			$("<div></div>").attr("class", "panel-body").css("padding", "0").append(
+                $("<h2></h2>").html(pitem.name)
+            ),
+            $("<div></div>").attr("class", "panel-footer").append(
+                $("<p></p>").html(pitem.desc),
+                $("<a></a>").attr("class", "btn btn-mini return").attr("href", "index.html").html("return")
+            )
+            
 		)
 	)
 }
